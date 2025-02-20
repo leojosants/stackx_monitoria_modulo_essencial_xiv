32000abe69b437d2811c72107c2c5698c62a7474
Leonardo Santos <leonardojosantos@gmail.com>
2025-02-20 17:59:28 -0300

    Programação JavaScript -- Aula - Trabalhando com variáveis e tipos de dados

---
+++ 01__modulos/05__programacao_javascript/aula_trabalhando_com_variaveis_e_tipos_de_dados/05__parte_pratica/exemplo_01.js
@@ -0,0 +1,17 @@
+
+let nome = "Maria";
+let idade = 25;
+const PI = 3.14;
+let indefinido;
+
+console.log(nome, "");
+console.log("Tipo de dado ..: ", typeof (nome), "\n");
+
+console.log(idade);
+console.log("Tipo de dado ..: ", typeof (idade), "\n");
+
+console.log(PI);
+console.log("Tipo de dado ..: ", typeof (PI), "\n");
+
+console.log(indefinido);
+console.log("Tipo de dado ..: ", typeof (indefinido), "\n");
